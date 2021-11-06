085cf6738b57e69988ed2de6dde5c4d1a889e57d
hrustalevanv <hrustalevanv2021@gmail.com>
2021-11-06 15:30:55 +0300

    Hall 3

---
+++ Task006/Program.cs
@@ -0,0 +1,10 @@
+﻿//6. Выяснить является ли число чётным
+int one = 14;
+if(one % 2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
---
+++ Task007/Program.cs
@@ -0,0 +1,7 @@
+﻿//7. Показать числа от -N до N
+int start = -10;
+int end = 10;
+for (int i = start; i <= end; i++)
+{
+    Console.Write($"{i} ");
+}
