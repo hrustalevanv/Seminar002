0f1f2657d0fdeb65de3e59ec549069745b07ccba
hrustalevanv <hrustalevanv2021@gmail.com>
2021-12-04 17:50:15 +0300

    Task009

---
+++ Task009/Program.cs
@@ -0,0 +1,8 @@
+﻿//9.Показать последнюю цифру трёхзначного числа 
+int a = 123;
+ void Main(int arg)
+{
+    string s = arg.ToString();
+    Console.WriteLine ($"Выводим последнюю цифру {s[2]}" );
+}
+Main(a);
