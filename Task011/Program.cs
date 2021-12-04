4b2bb728e226daa7fcb0defc11f06d7d2dbdea31
hrustalevanv <hrustalevanv2021@gmail.com>
2021-12-04 19:44:21 +0300

    Task011

---
+++ Task011/Program.cs
@@ -0,0 +1,22 @@
+﻿//11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+void array1(int arg)
+{
+    string s = arg.ToString();
+
+    int i1 = (int)(s[0] -'0'); 
+    int i2 = (int)(s[1] -'0');
+
+   Console.WriteLine($"s[0] = {s[0]}");  
+   Console.WriteLine($"s[1] = {s[1]}");
+
+  if(i1 > i2)
+  {
+      Console.WriteLine($"Max = {i1}");
+  }
+
+  if(i2 > i1)
+  {
+      Console.WriteLine($"Max = {i2}");
+  }
+}
+array1(83);
