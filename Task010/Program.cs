7eceb47941f7aa74dd26592c2c38a388f13bbce0
hrustalevanv <hrustalevanv2021@gmail.com>
2021-12-04 17:59:22 +0300

    Task010

---
+++ Task010/Program.cs
@@ -0,0 +1,8 @@
+﻿//10. Показать вторую цифру трёхзначного числа
+int i = 565;
+void array(int Array)
+{
+    string a = Array.ToString();
+    Console.WriteLine($"Выводим вторую цифру {a[1]}" );
+}
+array(i);
