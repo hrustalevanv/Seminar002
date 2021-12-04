550d170b5833ea759ea90117fbda192bfa825fa5
hrustalevanv <hrustalevanv2021@gmail.com>
2021-12-04 16:23:57 +0300

    Task008

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -8,3 +8,4 @@ else
 {
     Console.WriteLine("Нечетное");
 }
+
---
+++ Task08/Program.cs
@@ -0,0 +1,8 @@
+﻿//8. Показать четные числа от 1 до N
+int start = 0;
+int end = 20;
+for (int i = start; i <= end; i++)
+{
+    if (i % 2 == 0)
+    Console.WriteLine($"{i} ");
+}
